de2c2a2517b588f82120ec0e36c4159a77ea1c8d
jaroslawpiotrowski <50195393+jaroslawpiotrowski@users.noreply.github.com>
2019-10-25 15:10:40 +0200

    VCCS-692 update lambda initialization (#45)

--- src/main/java/uk/gov/caz/tariff/Application.java
+++ src/main/java/uk/gov/caz/tariff/Application.java
@@ -1,4 +1,25 @@
 package uk.gov.caz.tariff;
 
-public class Application extends uk.gov.caz.Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import uk.gov.caz.tariff.configuration.RequestMappingConfiguration;
+import uk.gov.caz.tariff.configuration.SwaggerConfiguration;
+import uk.gov.caz.tariff.controller.CleanAirZonesController;
+import uk.gov.caz.tariff.service.CleanAirZonesRepository;
+import uk.gov.caz.tariff.service.TariffRepository;
+
+@Import({
+    RequestMappingConfiguration.class,
+    SwaggerConfiguration.class,
+    CleanAirZonesController.class,
+    TariffRepository.class,
+    CleanAirZonesRepository.class
+})
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
