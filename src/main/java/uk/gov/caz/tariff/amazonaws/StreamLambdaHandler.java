e3186d748a6c1ab1a862a629e88c2d9125c8f7a2
Tuan Anh Phan <phan.anh.tuan@gmail.com>
2019-11-08 12:38:26 +1100

    address code review comment

--- src/main/java/uk/gov/caz/tariff/amazonaws/StreamLambdaHandler.java
+++ src/main/java/uk/gov/caz/tariff/amazonaws/StreamLambdaHandler.java
@@ -10,11 +10,20 @@ import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Preconditions;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import org.springframework.util.StreamUtils;
 import uk.gov.caz.tariff.Application;
+import uk.gov.caz.tariff.controller.WarmupController;
+import uk.gov.caz.tariff.dto.LambdaContainerStats;
+
 
 public class StreamLambdaHandler implements RequestStreamHandler {
 
@@ -49,6 +58,24 @@ public class StreamLambdaHandler implements RequestStreamHandler {
   @Override
   public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
       throws IOException {
+    String input = StreamUtils.copyToString(inputStream, Charset.defaultCharset());
+    if (!isWarmupRequest(input)) {
+      LambdaContainerStats.setRequestTime(LocalDateTime.now());
+    }
     handler.proxyStream(inputStream, outputStream, context);
   }
+  
+  private boolean isWarmupRequest(String input) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode node;
+    try {
+      node = objectMapper.readTree(input);
+      JsonNode path = node.get("path");
+      Preconditions.checkNotNull(path);
+      return WarmupController.PATH.equalsIgnoreCase(path.textValue());
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
 }
