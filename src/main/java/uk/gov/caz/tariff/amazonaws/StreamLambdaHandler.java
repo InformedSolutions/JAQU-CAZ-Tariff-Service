77d09169d1ba40ba777b8de3f4a431d8658dc235
cruddasj <james.cruddas@informed.com>
2019-11-13 19:01:56 +0000

    Update streamhandler to use sync startup

--- src/main/java/uk/gov/caz/tariff/amazonaws/StreamLambdaHandler.java
+++ src/main/java/uk/gov/caz/tariff/amazonaws/StreamLambdaHandler.java
@@ -1,18 +1,14 @@
 package uk.gov.caz.tariff.amazonaws;
 
-import static uk.gov.caz.awslambda.AwsHelpers.splitToArray;
-
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.internal.LambdaContainerHandler;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
-import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.google.common.base.Splitter;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,63 +16,75 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
-
 import lombok.extern.slf4j.Slf4j;
-
 import org.jetbrains.annotations.NotNull;
 import org.springframework.util.StreamUtils;
-
 import uk.gov.caz.tariff.Application;
 
 @Slf4j
 public class StreamLambdaHandler implements RequestStreamHandler {
 
   private static final String KEEP_WARM_ACTION = "keep-warm";
-  private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+  private static final Splitter SPLITTER = Splitter.on(',')
+      .trimResults()
+      .omitEmptyStrings();
+  
+  private SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
-  static {
-    long startTime = Instant.now().toEpochMilli();
-    try {
-      // For applications that take longer than 10 seconds to start, use the async builder:
-      String listOfActiveSpringProfiles = System.getenv("SPRING_PROFILES_ACTIVE");
-      LambdaContainerHandler.getContainerConfig().setInitializationTimeout(50_000);
-      if (listOfActiveSpringProfiles != null) {
-        handler = new SpringBootProxyHandlerBuilder()
-            .defaultProxy()
-            .asyncInit(startTime)
-            .springBootApplication(Application.class)
-            .profiles(splitToArray(listOfActiveSpringProfiles))
-            .buildAndInitialize();
-      } else {
-        handler = new SpringBootProxyHandlerBuilder()
-            .defaultProxy()
-            .asyncInit(startTime)
-            .springBootApplication(Application.class)
-            .buildAndInitialize();
+  /**
+   * Private method for instantiating a Spring handler to serve the inbound
+   * request.
+   */
+  private void initialiseSpringHandler() {
+    if (handler == null) {
+      try {
+        String listOfActiveSpringProfiles = System.getenv("SPRING_PROFILES_ACTIVE");
+        if (listOfActiveSpringProfiles != null) {
+          handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class,
+              splitToArray(listOfActiveSpringProfiles));
+        } else {
+          handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+        }
+      } catch (ContainerInitializationException e) {
+        // if we fail here. We re-throw the exception to force another cold
+        // start
+        log.error(e.getMessage());
+        throw new RuntimeException(
+            "Could not initialize Spring Boot application", e);
       }
-    } catch (ContainerInitializationException e) {
-      // if we fail here. We re-throw the exception to force another cold start
-      throw new RuntimeException("Could not initialize Spring Boot application", e);
     }
   }
-
+  
+  /**
+   * Private helper for splitting a comma delimited string to an array.
+   */
+  private String[] splitToArray(String activeProfiles) {
+    return SPLITTER.splitToList(activeProfiles).toArray(new String[0]);
+  }
+  
   @Override
   public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
       throws IOException {
+    
+    // Initialise Spring handler under both real request conditions
+    // as well as warmup requests.
+    initialiseSpringHandler();
+
     byte[] inputBytes = StreamUtils.copyToByteArray(inputStream);
+
     if (isWarmupRequest(toString(inputBytes))) {
       delayToAllowAnotherLambdaInstanceWarming();
       try (Writer osw = new OutputStreamWriter(outputStream)) {
         osw.write(LambdaContainerStats.getStats());
       }
     } else {
+      log.info("Processing API request");
       LambdaContainerStats.setLatestRequestTime(LocalDateTime.now());
       handler.proxyStream(toInputStream(inputBytes), outputStream, context);
     }
@@ -132,7 +140,7 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     if (isWarmupRequest) {
       log.info("Received lambda warmup request");
     }
-    
+
     return isWarmupRequest;
   }
 
